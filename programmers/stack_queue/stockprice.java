6e5b7c4d96444ecc1ffd37804c2a3f3932d83617
lecto17 <khwoo_rpsit@naver.com>
2020-09-22 23:47:04 +0900

    2020-09-22

--- programmers/stack_queue/stockprice.java
+++ programmers/stack_queue/stockprice.java
@@ -2,6 +2,8 @@
 //
 // 실행만 맞고 나머지 케이스는 다 틀림
 // 10가지 이상 넣은 test case는 실행되는데, 실제 제출시에는 실패함..
+// 문제를 잘 못이해하고 있어서 그랬음,, **입출력 예 설명 해주는 부분을 꼼꼼이 읽을 것!!!!**
+//
 // import java.util.*;
 //
 //class Solution {
@@ -31,3 +33,54 @@
 //        return answer;
 //    }
 //}
+//
+// 문제 제대로 이해한 후 풀이..
+//class Solution {
+//    public int[] solution(int[] prices) {
+//        int len = prices.length;
+//        int[] answer = new int[len];
+//        int cnt = 0;
+//
+//        for(int i = 0; i < len; i++){
+//            for(int j = i+1; j < len; j++){
+//                cnt++;
+//                if(prices[i] > prices[j]){
+//                    answer[i] = cnt;
+//                    cnt = 0;
+//                    break;
+//                }
+//                if(j == len - 1) {
+//                    answer[i] = cnt;
+//                    cnt = 0;
+//                }
+//            }
+//        }
+//        return answer;
+//    }
+//}
+//
+//다른 사람 풀이
+//class Solution {
+//    public int[] solution(int[] prices) {
+//        int[] answer = new int[prices.length];
+//
+//        for(int i = 0; i < prices.length; i++)
+//        {
+//            for(int j=i+1; j < prices.length; j++)
+//            {
+//                if(prices[i] > prices[j])
+//                {
+//                    answer[i] = j-i;
+//                    break;
+//                }
+//                else
+//                    answer[i] = j-i;
+//            }
+//        }
+//
+//        //System.out.println(Arrays.toString(answer));
+//
+//        return answer;
+//    }
+//}
+

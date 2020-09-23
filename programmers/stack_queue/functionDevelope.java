34de6869b4387bc2314354535d765a467719b81f
lecto17 <khwoo_rpsit@naver.com>
2020-09-24 01:33:59 +0900

    2020-09-24

--- programmers/stack_queue/functionDevelope.java
+++ programmers/stack_queue/functionDevelope.java
@@ -1,25 +1,42 @@
 //기능개발
 //
 //
-//class Solution {
+//
+//import java.util.*;
+
+class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = null;
         int done = 100;
-        int[] remain = new int[progresses.length];
-        int check = (int)Math.ceil((done - progress[0]) / speeds[0]);
+        int len = progresses.length;
+        int[] remainTotal = new int[len];
+        int[] remainDays = new int[len];
         int cnt = 0;
-
-        while(true){
-            for(int i = 0; i < processes.length; i++){
-                remain[i] = done - progresses[i];
-                if(remain[0] - speeds[0]*(i+1) <= 0){
-
-                }
-                if(remain[i] - speeds[i] * (i+1) == 0)
-                    cnt++;
-            }
+        List<Integer> list = new ArrayList<>();
+                
+        for(int i = 0; i < len; i++){
+            remainTotal[i] = done - progresses[i];
+            remainDays[i] = (int)Math.ceil(remainTotal[i] / speeds[i]);
         }
-
+        
+        Arrays.sort(remainDays);
+        
+        for(int i = 0; i < remainDays[len - 1]; i++) {
+            for(int j = 0; j < len; j++){                                
+                if(remainTotal[j] - speeds[j]*(i+1) <= 0)
+                    cnt++;                                 
+            }   
+            if(remainTotal[0] - speeds[0]*(i+1) <= 0)
+                    list.add(cnt);                                
+            cnt = 0;
+        }        
+        
+        len = list.size();
+        
+        answer = new int[len];
+        for(int i = 0; i < len; i++)
+            answer[i] = list.get(i);
+            
         return answer;
     }
 }

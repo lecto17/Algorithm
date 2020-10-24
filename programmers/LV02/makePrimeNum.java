b26b39668006f12a808298c64ffe99a70d2830d5
lecto17 <khwoo_rpsit@naver.com>
2020-10-24 23:57:28 +0900

    2020-10-24

---
+++ programmers/LV02/makePrimeNum.java
@@ -0,0 +1,35 @@
+//소수 만들기
+//
+//import java.util.*;
+//
+//class Solution {
+//    public int solution(int[] nums) {
+//        int answer = 0, sum = 0;
+//        int len = nums.length;
+//        int count = (len * (len-1) * (len - 2)) / (3 * 2 * 1);
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        for(int i = 0; i < nums; i++){
+//            map.put(i, 0);
+//        }
+//
+//        for(int i = 0; i < count; i++){
+//            for(int j = 0;)
+//        }
+//
+//        return answer;
+//    }
+//
+//    public int findPrime(int num){
+//        int cnt = 0;
+//        for(int i = 2; i <= num; i++){
+//            if(num % i == 0){
+//               if(cnt >= 2)
+//                    return 0;
+//                else
+//                    cnt++;
+//            }
+//        }
+//        return 1;
+//    }
+//}

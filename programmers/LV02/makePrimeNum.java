444adca767c94ffd92f4cb8e6ea8f54b97c2742c
lecto17 <khwoo_rpsit@naver.com>
2020-10-25 00:07:47 +0900

    2020-10-25

--- programmers/LV02/makePrimeNum.java
+++ programmers/LV02/makePrimeNum.java
@@ -1,5 +1,46 @@
 //소수 만들기
 //
+//풀이 참고
+//
+//import java.util.*;
+//
+//class Solution {
+//    public int solution(int[] nums) {
+//        int answer = 0, sum = 0;
+//        int len = nums.length;        
+//        
+//        for(int i = 0; i < len; i++){
+//            for(int j = i + 1; j < len; j++){
+//                for(int k = j +1; k < len; k++){
+//                    sum = nums[i] + nums[j] + nums[k];
+//                    //System.out.println("sum : "+ sum+". answer: "+ answer);
+//                    answer += findPrime(sum);
+//                    //System.out.println("answer: "+ answer);
+//                }
+//            }
+//        }
+//        
+//        return answer;
+//    }
+//    
+//    public int findPrime(int num){
+//        int cnt = 0;
+//        for(int i = 2; i <= num; i++){
+//            if(num % i == 0){
+//               if(cnt >= 2)
+//                    return 0;  
+//                else if(num == i * i)
+//                    cnt += 2;
+//                else
+//                    cnt++;
+//            }  
+//        }
+//        return 1;
+//    }
+//}
+//
+//
+//
 //import java.util.*;
 //
 //class Solution {

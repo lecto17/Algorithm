a25c231c4fb05ef36ee1a88c4ba7be43a2152c66
lecto17 <khwoo_rpsit@naver.com>
2020-10-25 00:10:48 +0900

    2020-10-25

--- programmers/LV02/makePrimeNum.java
+++ programmers/LV02/makePrimeNum.java
@@ -1,6 +1,77 @@
 //소수 만들기
 //
-//풀이 참고
+//다른 사람 풀이
+//
+//class Solution {
+//    public static int result = 0;
+//    public int solution(int[] nums) {
+//        int answer = 0;
+//        int n = nums.length;
+//        int r = 3;
+//
+//        int[] arr = new int[n];
+//        combination(arr, 0, n, r, 0, nums);
+//        answer = result;
+//        //if(n == r || r == 0) 
+//          //  return result = 1;
+//        //else 
+//            //result = combination2(n - 1, r - 1) + combination(n - 1, r);
+//
+//        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//        //System.out.println("result : " + result);
+//
+//        return answer;
+//    }
+//
+//    public int combination2(int n, int r) {
+//
+//        if(n == r || r == 0) 
+//            return 1;
+//        else 
+//            return combination2(n - 1, r - 1) + combination2(n - 1, r);
+//
+//    }
+//
+//    public static void combination(int[] arr, int index, int n, int r, int target, int[] nums) { 
+//        if (r == 0) 
+//            print(arr, index); 
+//        else if (target == n) 
+//            return; 
+//        else { 
+//            arr[index] = nums[target]; 
+//            combination(arr, index + 1, n, r - 1, target + 1, nums); 
+//            combination(arr, index, n, r, target + 1, nums); 
+//        } 
+//    }//end combination() 
+//    public static void print(int[] arr, int length) { 
+//        int cnt = 0;
+//        boolean isPrime = false;
+//        for (int i = 0; i < length; i++) {
+//            cnt += arr[i];
+//            // System.out.print(arr[i]); 
+//
+//        }
+//        System.out.println("cnt :" + cnt);
+//         for (int i = 2; i < cnt; i++) {
+//            // 1과 num 자신 외에 나누어지는 수가 있는지 검사할 조건문
+//            if (cnt % i == 0) {
+//                // 나누어지는 수가 있을 경우 isPrime의 값을 true로 바꾼다.
+//                isPrime = true;
+//                // 한 번이라도 이 조건문이 실행될 경우 num은 소수가 아니므로 반복문을 빠져나온다.
+//                break;
+//            }
+//        }
+//
+//        if (!isPrime) {
+//            result++;
+//        }
+//
+//
+//
+//    }
+//
+//}
+//
 //
 //import java.util.*;
 //

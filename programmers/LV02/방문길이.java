67bdff8e6173637eda175f3bcb7c14e1e6f5557d
lecto17 <khwoo_rpsit@naver.com>
2020-10-30 23:03:11 +0900

    방문길이.java 보완

--- programmers/LV02/방문길이.java
+++ programmers/LV02/방문길이.java
@@ -1,3 +1,64 @@
+//어느 방향에서 왔는지 까지 체크해야함!
+//바로 위와 같이 7번처럼 방문이 안되야하는데 된걸로 되서 잘못풀었단거를 꺠달았습니다.
+
+
+
+//그래서 어디서 어디로 왔는지 알아야했습니다. 이를 4차원 배열을 이용해서 풀었습니다. 1,2 차원 지점에서 3,4차원지점으로 이동했다는 것을 기록합니다.
+//
+//문제 풀이방법이 더 간단해졌습니다.
+//참조 : https://youngest-programming.tistory.com/297
+//
+//class Solution {
+//    private static int answer = 0;
+//    private static boolean[][][][] isVisited = new boolean[11][11][11][11]; // (x,y) -> (x,y) 로 이동한 적 있는지
+//    private static int x, y; //현재좌표
+//    private static int x2, y2; //이동할좌표
+//    private static int[] dx = {-1, 0, 1, 0};
+//    private static int[] dy = {0, -1, 0, 1};
+//
+//    public int solution(String dirs) {
+//        x = 5;
+//        y = 5;
+//        x2 = 5;
+//        y2 = 5;
+//        for (char move : dirs.toCharArray()) {
+//            x = x2;
+//            y = y2;
+//            if (move == 'L') { //좌하우상
+//                x2 = x + dx[0];
+//                y2 = y + dy[0];
+//            } else if (move == 'D') {
+//                x2 = x + dx[1];
+//                y2 = y + dy[1];
+//            } else if (move == 'R') {
+//                x2 = x + dx[2];
+//                y2 = y + dy[2];
+//            } else {
+//                x2 = x + dx[3];
+//                y2 = y + dy[3];
+//            }
+//
+//            //범위벗어난거처리
+//            if (x2 < 0 || x2 > 10 || y2 < 0 || y2 > 10) {
+//                //이동하기 전 좌표로 바꿔줌
+//                x2 = x;
+//                y2 = y;
+//                continue;
+//            }
+//
+//            //이동가능한 좌표, 첫방문이면 답 +1
+//            if (!isVisited[x][y][x2][y2]) {
+//                isVisited[x][y][x2][y2] = true;
+//                isVisited[x2][y2][x][y] = true;
+//                answer++;
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
+//
+
 // 방문했던 점을 for 문안에서 언제 map에 넣어주는지
 
 import java.util.*;

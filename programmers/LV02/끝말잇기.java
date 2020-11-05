b264d8efced1776407b12b25b0124b5fb373d2f2
lecto17 <khwoo_rpsit@naver.com>
2020-11-05 14:41:46 +0900

    2020-11-05

--- programmers/LV02/끝말잇기.java
+++ programmers/LV02/끝말잇기.java
@@ -1,3 +1,51 @@
+//20가지 중 4가지 실패(한 자리 단어 보완)
+
+import java.util.*;
+    
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        int len = -1;        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < words.length; i++){
+            len = words[i].length();
+            if(map.get(words[i]) == null)
+                map.put(words[i], 1);
+            else{
+                //같은 단어를 했을 경우
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }                
+            
+            //한 글자 단어를 말했을 경우
+            if(words[i].length() == 1){
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
+            }                
+            
+            //다른 글자로 단어를 이을 경우
+            if(i < words.length-1 && !words[i].substring(len-1, len).equals(words[i+1].substring(0,1))){
+                answer[0] = (++i % n) + 1;
+                answer[1] = (++i / n) + 1;
+                break;
+            } 
+            
+            //탈락자가 없을 경우
+            if(i == words.length-1)
+            {
+                answer[0] = 0;
+                answer[1] = 0;
+            }
+        }
+        //map.forEach((key, value) -> System.out.println("key: "+ key+", value: "+ value));
+        
+        return answer;
+    }
+}
+
 //20가지 중 6가지 경우 실패
 import java.util.*;
     
@@ -23,7 +71,7 @@ class Solution {
             }
             //같은 단어를 했을 경우
             if(map.get(words[i]) != 1){
-                answer[0] = (i % n) + 1;
+                answer[1] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
             }                      

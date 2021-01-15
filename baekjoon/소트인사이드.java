c530e7e3b26823d4b700a039cc8f0bbdf3ebd86b
lecto17 <khwoo_rpsit@naver.com>
2021-01-15 19:53:02 +0900

    소트인사이트(1)

--- baekjoon/소트인사이드.java
+++ baekjoon/소트인사이드.java
@@ -29,3 +29,34 @@ public class Test {
         sc.close();
     }
 }
+
+
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long n ;
+        n = sc.nextLong();
+        String temp = Long.toString(n);
+
+        int len = temp.length();
+
+        Integer[] arr = new Integer[len];
+
+        for(int i = 0; i < len; i++){
+            arr[i] = Integer.parseInt(temp.substring(i, i+1));
+        }
+
+        temp = "";
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(Integer i : arr)
+            temp += i;
+
+        sc.close();
+
+        System.out.println(temp);
+    }
+}

0a8aa476a7d6361475cf95cb3fd741ffc3161a4d
lecto17 <khwoo_rpsit@naver.com>
2021-01-15 19:10:51 +0900

    수정렬하기

---
+++ baekjoon/수정렬하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Test {            
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n ;
+        n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        for(int i : arr)
+            System.out.println(i);
+        
+        sc.close();
+    }
+}

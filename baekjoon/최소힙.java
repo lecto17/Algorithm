eab839acae58c2973af159ec920a67b95df97bc1
lecto17 <khwoo_rpsit@naver.com>
2021-02-03 00:55:37 +0900

    시간초 통과

--- baekjoon/최소힙.java
+++ baekjoon/최소힙.java
@@ -1,3 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());        
+        int temp;
+        StringBuffer sb = new StringBuffer();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while(n-- > 0){
+            temp = Integer.parseInt(br.readLine());
+            if(temp == 0){
+                if(pq.size() == 0)
+                    sb.append("0\n");
+                else
+                    sb.append(pq.poll()+"\n");
+            }
+            else{
+                pq.add(temp);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
+
 //시간초에서 실패.. 
 //시간 제한은 1초, 평균 채점 시간은 8초
 

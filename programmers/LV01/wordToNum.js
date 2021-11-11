0e602113bb04168910401b436eb6433fb3ba0671
lecto17 <khwoo_rpsit@naver.com>
2021-11-11 23:42:39 +0900

    숫자 문자열과 영단어

---
+++ programmers/LV01/wordToNum.js
@@ -0,0 +1,49 @@
+function check(str) {
+    switch (str) {
+        case "zero":
+            return '0';
+        case "one":
+            return '1';
+        case "two":
+            return '2';
+        case "three":
+            return '3';
+        case "four":
+            return '4';
+        case "five":
+            return '5';
+        case "six":
+            return '6';
+        case "seven":
+            return '7';
+        case "eight":
+            return '8';
+        case "nine":
+            return '9';
+        default:
+            return "";
+
+    }
+}
+
+function solution(s) {
+    var answer = "";
+    let temp = "";
+    let tmp = "";
+
+    for (let i=0; i<s.length; i++) {
+        temp = s.slice(i, i+1);
+        if (temp*1 >= 0) {
+            answer += temp;
+
+        } else {
+            tmp += temp;
+            if (check(tmp)) {
+                answer += check(tmp);
+                tmp = "";
+            }
+        }
+    }
+
+    return answer*1;
+}

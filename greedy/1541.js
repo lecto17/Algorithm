2215f9465cea0bc9b3ed630361a209f6341c7402
lecto17 <khwoo_rpsit@naver.com>
2024-01-16 20:35:55 +0900

    괄호넣기

--- greedy/1541.js
+++ greedy/1541.js
@@ -1,29 +1,52 @@
 /**
  * 1541 번
  * [ 테스트 케이스 ]
- * 10+30
- * 10+30-20
- * 10-30+20
- * 10-30-20
+ * 10+30 => 40
+ * 10+30-20 => 20
+ * 10-30+20 => -40
+ * 10-30+20+10 => -50
+ * 10-30-20 => -40
  *
- * 10-20-30-40-50
- * 10-20+30-40+50
- * 0+3+20-30-10+50-35
+ * 10-20-30-40-50 => -130
+ * 10-20+30-40+50 => -130
+ * 0+3+20-30-10+50-35 => -102
+ * 0+3+20-30-10+20+50-35 => -122
  *
  * [ 풀이 방법 ]
  * 1. - 시작 + 인 숫자들은 하나의 괄호로 한다.
  * 2. - 으로 시작하고 +가 중간에 있다가 -를 다시 만나게 되면 괄호를 - 전에 닫고 새로운 괄호를 생성한다.
  */
 
-function solution(num) {
-  let answer = "";
-  let total = num;
+function solution(numbers, chars) {
+  let alreadyMeetMinusFlag = false;
+  let sum = 0,
+    sumOfBracket = 0;
 
-  for (const balance of balances) {
-    answer += `${Math.floor(total / balance)} `;
-    total -= Math.floor(total / balance) * balance;
+  sum += parseInt(numbers.shift());
+  for (let i = 0; i < chars.length; i++) {
+    if (chars.charAt(i) === "+") {
+      if (alreadyMeetMinusFlag) {
+        sumOfBracket += parseInt(numbers.shift());
+
+        if (chars.charAt(i + 1) === "+") {
+          continue;
+        }
+        sum -= sumOfBracket;
+        sumOfBracket = 0;
+        alreadyMeetMinusFlag = false;
+        continue;
+      }
+
+      sum += parseInt(numbers.shift());
+      continue;
+    }
+
+    // char is '-'
+    alreadyMeetMinusFlag = true;
+    sumOfBracket += parseInt(numbers.shift());
   }
-  return answer;
+
+  console.log(sum - sumOfBracket);
 }
 
 const readline = require("readline");
@@ -38,7 +61,8 @@ rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
   // 숫자, 문자 배열을 각각 갖고 있는게 좋을까?
-  const numbers = input[0].split();
-  solution();
+  const numbers = input[0].split(/[+,-]/);
+  const chars = input[0].replace(/\d/g, "");
+  solution(numbers, chars);
   process.exit();
 });
